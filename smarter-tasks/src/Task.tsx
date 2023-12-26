47b53c6ea02fa8b2ea231530f461c7e201c2c873
Gowtham-Neo <s.gowtham.engr@gmail.com>
2023-12-26 21:33:05 +0530

    deleted unused declaration

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./TaskCard.css";
 import { TaskItem } from "./types";
 
--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { TaskItem } from "./types";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
