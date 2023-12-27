9c9793439eb3af6ba64b9c805b120a92e3ffe685
Gowtham-Neo <s.gowtham.engr@gmail.com>
2023-12-27 10:38:19 +0530

    Route Handling

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css';
 import TaskApp from "./TaskApp";
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />,
+  },
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 export default App;
---
+++ smarter-tasks/src/pages/TaskListPage.tsx
@@ -0,0 +1,12 @@
+// TaskListPage.tsx
+import React from 'react';
+import TaskApp from '../TaskApp';
+const TaskListPage: React.FC = () => {
+  return (
+    <div>
+      <h1>This is the Task List Page</h1>
+    </div>
+  );
+};
+
+export default TaskListPage;
