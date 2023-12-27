8bd04a4ed20c7a5c8c21508aa5044e935336d327
Gowtham-Neo <s.gowtham.engr@gmail.com>
2023-12-27 13:21:22 +0530

    Notfound err

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -4,7 +4,7 @@ const NotFound: React.FC = () => {
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="text-center">
                 <h1 className="mb-4 text-4xl font-bold text-red-500">404 - Not Found</h1>
-                <a href="/" className="text-blue-500 hover:underline">Go to Home Page</a>
+                <a href="/home" className="text-blue-500 hover:underline">Go to Home Page</a>
             </div>
         </div>
     );
