bb80bd222139604abb360823c0b644493163a023
Gowtham-Neo <s.gowtham.engr@gmail.com>
2023-12-29 13:26:42 +0530

    Dashboard update

--- smarter-tasks/src/ProtectedRoute.tsx
+++ smarter-tasks/src/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 const authToken = localStorage.getItem('authToken');
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const authenticated = !!authToken
+  const authenticated = !! authToken
   if (authenticated) {
     return <>{children}</>;
   } else {
